77e557dc670164c4e4d8e87ce3811776ce0e177c
AlexGralewski <gralewski.aleksander@gmail.com>
2020-12-22 22:33:58 +0100

    Small tweaks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,9 +4,10 @@ class Calculator extends Component {
   constructor() {
     super()
     this.state = {
-      result: 0,
-      memory: "",
+      result: "0",
+      operationInput: "",
       operation: "",
+      memory: ""
     }
     this.handleEqualsSignClick = this.handleEqualsSignClick.bind(this)
     this.handleOperationSignClick = this.handleOperationSignClick.bind(this)
@@ -19,17 +20,24 @@ class Calculator extends Component {
   handleChange(event) {
     const { name, value } = event.target
     this.setState({
-      [name]: value,
+      [name]: value
     })
   }
 
   handleNumberClick(event) {
     const { name, value } = event.target
     this.setState(prevState => {
-      return ({
-        [name]: parseFloat(prevState.result) * 10 + parseFloat(value)
+      if (prevState.result === "0") {
+        return ({
+          [name]: value
+        })
+      } else {
+
+        return ({
+          [name]: prevState.result + value
+        })
       }
-      )
+      
     })
   }
 
@@ -42,21 +50,21 @@ class Calculator extends Component {
       if (value === "2√") {
         return({
           result: Math.sqrt(parseFloat(prevState.result)),
-          memory: ""
+          operationInput: ""
           })
       } else if (value === "3√") {
         return({
           result: Math.cbrt(parseFloat(prevState.result)),
-          memory: ""
+          operationInput: ""
           })
       } else if (value === "1/n") {
         return({
           result: 1 / prevState.result,
-          memory: ""
+          operationInput: ""
           })
       } else {
         return ({
-          memory: prevState.result,
+          operationInput: prevState.result,
           result: '0',
           [name]: value
         })
@@ -71,32 +79,32 @@ class Calculator extends Component {
     this.setState(prevState => {
       if (this.state.operation === "+") {
         return ({
-          result: parseFloat(prevState.result) + parseFloat(this.state.memory),
-          memory: "",
+          result: parseFloat(prevState.result) + parseFloat(this.state.operationInput),
+          operationInput: "",
           operation: ""
         })
       } else if (this.state.operation === "-") {
         return ({
-          result: parseFloat(prevState.memory) - parseFloat(this.state.result),
-          memory: "",
+          result: parseFloat(prevState.operationInput) - parseFloat(this.state.result),
+          operationInput: "",
           operation: ""
         })
       } else if (this.state.operation === "x") {
         return ({
-          result: parseFloat(prevState.result) * parseFloat(this.state.memory),
-          memory: "",
+          result: parseFloat(prevState.result) * parseFloat(this.state.operationInput),
+          operationInput: "",
           operation: ""
         })
       } else if (this.state.operation === "/") {
         return ({
-          result: parseFloat(prevState.memory) / parseFloat(this.state.result),
-          memory: "",
+          result: parseFloat(prevState.operationInput) / parseFloat(this.state.result),
+          operationInput: "",
           operation: ""
         })
       } else if (this.state.operation === "^") {
         return ({
-          result: parseFloat(prevState.memory) ** parseFloat(this.state.result),
-          memory: "",
+          result: parseFloat(prevState.operationInput) ** parseFloat(this.state.result),
+          operationInput: "",
           operation: ""
         })
       }
@@ -115,7 +123,7 @@ class Calculator extends Component {
   handleResetClick() {
     this.setState({
       result: "0",
-      memory: "0",
+      operationInput: "0",
       operation: ""
     })
   }
@@ -129,156 +137,167 @@ class Calculator extends Component {
   }
 
   render() {
-    const { result, memory, operation } = this.state
+    const { result, operationInput, memory, operation } = this.state
 
     return (
-      <div>
-        <form className="calculator">
-          <div className="display">
-            <div className="result">
-              {result}
-            </div>
-
-            <div className="memory">
-              {memory} {operation}
-            </div>
+      <form className="calculator">
+        <div className="display">
+          <div className="operation-input">
+            {operationInput} {operation}
           </div>
+          <div className="result">
+            {result}
+          </div>
+
+        </div>
+
+        <div className="top-part">
           <button className="" onClick={this.handleResetClick}>C</button>
           <button className="backspace" onClick={this.handleBackspaceClick}>Backspace</button>
-
-          <div className="top-part">
-            <input
-              className="operation"
-              type="button"
-              name="operation"
-              value="^"
-              onClick={this.handleOperationSignClick} />
-            <input
-              className="operation"
-              type="button"
-              name="operation"
-              value="2√"
-              onClick={this.handleOperationSignClick} />
-            <input
-              className="operation"
-              type="button"
-              name="operation"
-              value="1/n"
-              onClick={this.handleOperationSignClick} />
-            <input
-              className="operation"
-              type="button"
-              name="operation"
-              value="/"
-              onClick={this.handleOperationSignClick} />
-          </div>
-
-          <div className="bottom-part">
-            <div className="numbers">
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="7"
-                onClick={this.handleNumberClick} />
-
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="8"
-                onClick={this.handleNumberClick} />
-
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="9"
-                onClick={this.handleNumberClick} />
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="4"
-                onClick={this.handleNumberClick} />
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="5"
-                onClick={this.handleNumberClick} />
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="6"
-                onClick={this.handleNumberClick} />
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="1"
-                onClick={this.handleNumberClick} />
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="2"
-                onClick={this.handleNumberClick} />
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="3"
-                onClick={this.handleNumberClick} />
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="+/-"
-                onClick={this.handlePositiveNegativeNumberChange} />
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="0"
-                onClick={this.handleNumberClick} />
-              <input
-                className="number-button"
-                type="button"
-                name="result"
-                value="."
-                onClick={this.handleNumberClick} />
-            </div>
-            <div className="operations">
-              <input
-                className="operation"
-                type="button"
-                name="operation"
-                value="-"
-                onClick={this.handleOperationSignClick} />
-              <input
-                className="operation"
-                type="button"
-                name="operation"
-                value="+"
-                onClick={this.handleOperationSignClick} />
-              <input
-                className="operation"
-                type="button"
-                name="operation"
-                value="x"
-                onClick={this.handleOperationSignClick} />
-              <button className="operation" onClick={this.handleEqualsSignClick}>=</button>
-            </div>
-          </div>
-        </form>
-        <div>
-          Result: {result}
-          Memory: {memory}
-          Operation: {operation}
+          <input
+            className="operation"
+            type="button"
+            name="operation"
+            value="^"
+            onClick={this.handleOperationSignClick} />
+          <input
+            className="operation"
+            type="button"
+            name="operation"
+            value="2√"
+            onClick={this.handleOperationSignClick} />
+          <input
+            className="operation"
+            type="button"
+            name="operation"
+            value="3√"
+            onClick={this.handleOperationSignClick} />
+          <input
+            className="operation"
+            type="button"
+            name="operation"
+            value="^2"
+            onClick={this.handleOperationSignClick} />
+          <input
+            className="operation"
+            type="button"
+            name="operation"
+            value="^3"
+            onClick={this.handleOperationSignClick} />
+          <input
+            className="operation"
+            type="button"
+            name="operation"
+            value="1/n"
+            onClick={this.handleOperationSignClick} />
+          <input
+            className="operation"
+            type="button"
+            name="operation"
+            value="/"
+            onClick={this.handleOperationSignClick} />
         </div>
-      </div>
+
+        <div className="bottom-part">
+          <div className="numbers">
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="7"
+              onClick={this.handleNumberClick} />
+
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="8"
+              onClick={this.handleNumberClick} />
+
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="9"
+              onClick={this.handleNumberClick} />
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="4"
+              onClick={this.handleNumberClick} />
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="5"
+              onClick={this.handleNumberClick} />
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="6"
+              onClick={this.handleNumberClick} />
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="1"
+              onClick={this.handleNumberClick} />
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="2"
+              onClick={this.handleNumberClick} />
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="3"
+              onClick={this.handleNumberClick} />
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="+/-"
+              onClick={this.handlePositiveNegativeNumberChange} />
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="0"
+              onClick={this.handleNumberClick} />
+            <input
+              className="number-button"
+              type="button"
+              name="result"
+              value="."
+              onClick={this.handleNumberClick} />
+          </div>
+          <div className="operations">
+            <input
+              className="operation"
+              type="button"
+              name="operation"
+              value="-"
+              onClick={this.handleOperationSignClick} />
+            <input
+              className="operation"
+              type="button"
+              name="operation"
+              value="+"
+              onClick={this.handleOperationSignClick} />
+            <input
+              className="operation"
+              type="button"
+              name="operation"
+              value="x"
+              onClick={this.handleOperationSignClick} />
+            <button className="operation" onClick={this.handleEqualsSignClick}>=</button>
+          </div>
+        </div>
+      </form>
     )
   }
 }
