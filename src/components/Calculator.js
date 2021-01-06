879646b7ec53095471bb83fd7eb8a6ab9db790c3
AlexGralewski <gralewski.aleksander@gmail.com>
2021-01-06 21:32:38 +0100

    Small changes to readability

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,166 +1,177 @@
 import React, { Component } from "react"
 
 class Calculator extends Component {
-constructor() {
-  super()
-  this.state = {
-    result: '0',
-    input1: '0',
-    input2: '',
-    operation: "",
+  constructor() {
+    super()
+    this.state = {
+      result: '0',
+      input1: '0',
+      input2: '',
+      operation: "",
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleNumberClick = this.handleNumberClick.bind(this)
   }
-  this.handleChange = this.handleChange.bind(this)
-  this.handleSubmit = this.handleSubmit.bind(this)
-  this.handleNumberClick = this.handleNumberClick.bind(this)
-}
 
-handleChange(event) {
-  const { name, value } = event.target
-  this.setState({
-    [name]: value,
-  })
-}
+  handleChange(event) {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value,
+    })
+  }
 
-handleNumberClick(event) {
-  const { name, value } = event.target
-  this.setState(prevState => {
-    if (value === '.') {
-      console.log("clicked")
-      return ({
-        [name]: `${prevState.result}.`
-      })
-    }
-    if (prevState.result == '0') {
-      return ({
-        [name]: value
-      })
-    } else {
-      return ({
-        [name]: prevState.result + value
+  handleNumberClick(event) {
+    const { name, value } = event.target
+    this.setState(prevState => {
+      if (value === '.') {
+        console.log("clicked")
+        return ({
+          [name]: `${prevState.result}.`
+        })
       }
-      )
-    }
-  })
-}
+      if (prevState.result == '0') {
+        return ({
+          [name]: value
+        })
+      } else {
+        return ({
+          [name]: prevState.result + value
+        }
+        )
+      }
+    })
+  }
 
-handleSubmit(event) {
-  event.preventDefault()
+  handleSubmit(event) {
+    event.preventDefault()
 
-  this.setState({
-      result: this.state.input
-  })
-}
+    this.setState({
+        result: this.state.input
+    })
+  }
 
-render() {
-  return (
-    <div>
-      <form className="calculator">
-        <input
-          className="display"
-          type="number"
-          name="result"
-          value={this.state.result}
-          onChange={this.handleChange}
-        />
 
-        <div className="top-part">
-          <button onClick={this.handleSubmit}>1/x</button>
-          <button onClick={this.handleSubmit}>^</button>
-          <button onClick={this.handleSubmit}>%</button>
-          <button onClick={this.handleSubmit}>/</button>
+  render() {
+    const {handleChange, handleSubmit, handleNumberClick} = this
+    const {result} = this.state
+    return (
+      <div>
+        <form className="calculator">
+          <input
+            className="display"
+            type="number"
+            name="result"
+            value={result}
+            onChange={handleChange}
+          />
 
-        </div>
-
-        <div className="bottom-part">
-          <div className="numbers">
-            <input
-              className="number-button"
+          <div className="memory-buttons">
+            <input 
+              className="memory-button"
               type="button"
-              name="result"
-              value="7"
-              onClick={this.handleNumberClick} 
+              name="memory"
+              onClick={}
               />
-
-            <input
-              className="number-button"
-              type="button"
-              name="result"
-              value="8"
-              onClick={this.handleNumberClick} />
-
-            <input
-              className="number-button"
-              type="button"
-              name="result"
-              value="9"
-              onClick={this.handleNumberClick} />
-            <input
-              className="number-button"
-              type="button"
-              name="result"
-              value="4"
-              onClick={this.handleNumberClick} />
-            <input
-              className="number-button"
-              type="button"
-              name="result"
-              value="5"
-              onClick={this.handleNumberClick} />
-            <input
-              className="number-button"
-              type="button"
-              name="result"
-              value="6"
-              onClick={this.handleNumberClick} />
-            <input
-              className="number-button"
-              type="button"
-              name="result"
-              value="1"
-              onClick={this.handleNumberClick} />
-            <input
-              className="number-button"
-              type="button"
-              name="result"
-              value="2"
-              onClick={this.handleNumberClick} />
-            <input
-              className="number-button"
-              type="button"
-              name="result"
-              value="3"
-              onClick={this.handleNumberClick} />
-            <input
-              className="number-button"
-              type="button"
-              name="result"
-              value="+/-"
-              onClick={this.handleNumberClick} />
-            <input
-              className="number-button"
-              type="button"
-              name="result"
-              value="0"
-              onClick={this.handleNumberClick} />
-            <input
-              className="number-button"
-              type="button"
-              name="result"
-              value="."
-              onClick={this.handleNumberClick} />
           </div>
-          <div className="operations">
-            <button className="operation" onClick={this.handleSubmit}>-</button>
-            <button className="operation" onClick={this.handleSubmit}>+</button>
-            <button className="operation" onClick={this.handleSubmit}>x</button>
-            <button className="operation" onClick={this.handleSubmit}>=</button>
-          </div>
-        </div>
-      </form>
+          <div className="top-part">
+            <button onClick={handleSubmit}>1/x</button>
+            <button onClick={handleSubmit}>^</button>
+            <button onClick={handleSubmit}>%</button>
+            <button onClick={handleSubmit}>/</button>
 
-    </div>
-  )
-}
+          </div>
+
+          <div className="bottom-part">
+            <div className="numbers">
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="7"
+                onClick={handleNumberClick} 
+                />
+
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="8"
+                onClick={handleNumberClick} />
+
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="9"
+                onClick={handleNumberClick} />
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="4"
+                onClick={handleNumberClick} />
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="5"
+                onClick={handleNumberClick} />
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="6"
+                onClick={handleNumberClick} />
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="1"
+                onClick={handleNumberClick} />
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="2"
+                onClick={handleNumberClick} />
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="3"
+                onClick={handleNumberClick} />
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="+/-"
+                onClick={handleNumberClick} />
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="0"
+                onClick={handleNumberClick} />
+              <input
+                className="number-button"
+                type="button"
+                name="result"
+                value="."
+                onClick={handleNumberClick} />
+            </div>
+            <div className="operations">
+              <button className="operation" onClick={handleSubmit}>-</button>
+              <button className="operation" onClick={handleSubmit}>+</button>
+              <button className="operation" onClick={handleSubmit}>x</button>
+              <button className="operation" onClick={handleSubmit}>=</button>
+            </div>
+          </div>
+        </form>
+
+      </div>
+    )
+  }
 }
 
 export default Calculator;
